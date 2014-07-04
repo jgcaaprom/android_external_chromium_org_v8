c983120c61b9d94964bd7594a83f4e46044a2116
Android Chromium Automerger <chromium-automerger@android>
2014-07-04 11:09:05 +0000

    Update makefiles after merge of Chromium at 281381
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I2b78b60695ede530ee4949ea6472fbf644adf745

--- tools/gyp/v8_libplatform.host.linux-arm.mk
+++ tools/gyp/v8_libplatform.host.linux-arm.mk
@@ -3,7 +3,7 @@
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-LOCAL_MODULE := v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+LOCAL_MODULE := v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 LOCAL_MODULE_SUFFIX := .a
 LOCAL_MODULE_TAGS := optional
 LOCAL_IS_HOST_MODULE := true
@@ -25,168 +25,9 @@ LOCAL_GENERATED_SOURCES :=
 GYP_COPIED_SOURCE_ORIGIN_DIRS :=
 
 LOCAL_SRC_FILES := \
-	v8/src/accessors.cc \
-	v8/src/allocation.cc \
-	v8/src/allocation-site-scopes.cc \
-	v8/src/allocation-tracker.cc \
-	v8/src/api.cc \
-	v8/src/arguments.cc \
-	v8/src/assembler.cc \
-	v8/src/assert-scope.cc \
-	v8/src/ast-value-factory.cc \
-	v8/src/ast.cc \
-	v8/src/bignum-dtoa.cc \
-	v8/src/bignum.cc \
-	v8/src/bootstrapper.cc \
-	v8/src/builtins.cc \
-	v8/src/cached-powers.cc \
-	v8/src/checks.cc \
-	v8/src/code-stubs.cc \
-	v8/src/code-stubs-hydrogen.cc \
-	v8/src/codegen.cc \
-	v8/src/compilation-cache.cc \
-	v8/src/compiler.cc \
-	v8/src/contexts.cc \
-	v8/src/conversions.cc \
-	v8/src/counters.cc \
-	v8/src/cpu-profiler.cc \
-	v8/src/data-flow.cc \
-	v8/src/date.cc \
-	v8/src/dateparser.cc \
-	v8/src/debug.cc \
-	v8/src/deoptimizer.cc \
-	v8/src/disassembler.cc \
-	v8/src/diy-fp.cc \
-	v8/src/dtoa.cc \
-	v8/src/elements-kind.cc \
-	v8/src/elements.cc \
-	v8/src/execution.cc \
-	v8/src/extensions/externalize-string-extension.cc \
-	v8/src/extensions/free-buffer-extension.cc \
-	v8/src/extensions/gc-extension.cc \
-	v8/src/extensions/statistics-extension.cc \
-	v8/src/extensions/trigger-failure-extension.cc \
-	v8/src/factory.cc \
-	v8/src/fast-dtoa.cc \
-	v8/src/field-index.cc \
-	v8/src/fixed-dtoa.cc \
-	v8/src/flags.cc \
-	v8/src/frames.cc \
-	v8/src/full-codegen.cc \
-	v8/src/func-name-inferrer.cc \
-	v8/src/gdb-jit.cc \
-	v8/src/global-handles.cc \
-	v8/src/handles.cc \
-	v8/src/heap-profiler.cc \
-	v8/src/heap-snapshot-generator.cc \
-	v8/src/heap.cc \
-	v8/src/hydrogen-bce.cc \
-	v8/src/hydrogen-bch.cc \
-	v8/src/hydrogen-canonicalize.cc \
-	v8/src/hydrogen-check-elimination.cc \
-	v8/src/hydrogen-dce.cc \
-	v8/src/hydrogen-dehoist.cc \
-	v8/src/hydrogen-environment-liveness.cc \
-	v8/src/hydrogen-escape-analysis.cc \
-	v8/src/hydrogen-instructions.cc \
-	v8/src/hydrogen.cc \
-	v8/src/hydrogen-gvn.cc \
-	v8/src/hydrogen-infer-representation.cc \
-	v8/src/hydrogen-infer-types.cc \
-	v8/src/hydrogen-load-elimination.cc \
-	v8/src/hydrogen-mark-deoptimize.cc \
-	v8/src/hydrogen-mark-unreachable.cc \
-	v8/src/hydrogen-osr.cc \
-	v8/src/hydrogen-range-analysis.cc \
-	v8/src/hydrogen-redundant-phi.cc \
-	v8/src/hydrogen-removable-simulates.cc \
-	v8/src/hydrogen-representation-changes.cc \
-	v8/src/hydrogen-sce.cc \
-	v8/src/hydrogen-store-elimination.cc \
-	v8/src/hydrogen-types.cc \
-	v8/src/hydrogen-uint32-analysis.cc \
-	v8/src/i18n.cc \
-	v8/src/icu_util.cc \
-	v8/src/ic.cc \
-	v8/src/incremental-marking.cc \
-	v8/src/interface.cc \
-	v8/src/interpreter-irregexp.cc \
-	v8/src/isolate.cc \
-	v8/src/jsregexp.cc \
-	v8/src/lithium-allocator.cc \
-	v8/src/lithium-codegen.cc \
-	v8/src/lithium.cc \
-	v8/src/liveedit.cc \
-	v8/src/log-utils.cc \
-	v8/src/log.cc \
-	v8/src/lookup.cc \
-	v8/src/mark-compact.cc \
-	v8/src/messages.cc \
-	v8/src/objects-debug.cc \
-	v8/src/objects-printer.cc \
-	v8/src/objects-visiting.cc \
-	v8/src/objects.cc \
-	v8/src/optimizing-compiler-thread.cc \
-	v8/src/ostreams.cc \
-	v8/src/parser.cc \
-	v8/src/preparse-data.cc \
-	v8/src/preparser.cc \
-	v8/src/prettyprinter.cc \
-	v8/src/profile-generator.cc \
-	v8/src/property.cc \
-	v8/src/regexp-macro-assembler-irregexp.cc \
-	v8/src/regexp-macro-assembler-tracer.cc \
-	v8/src/regexp-macro-assembler.cc \
-	v8/src/regexp-stack.cc \
-	v8/src/rewriter.cc \
-	v8/src/runtime-profiler.cc \
-	v8/src/runtime.cc \
-	v8/src/safepoint-table.cc \
-	v8/src/sampler.cc \
-	v8/src/scanner-character-streams.cc \
-	v8/src/scanner.cc \
-	v8/src/scopeinfo.cc \
-	v8/src/scopes.cc \
-	v8/src/serialize.cc \
-	v8/src/snapshot-source-sink.cc \
-	v8/src/spaces.cc \
-	v8/src/store-buffer.cc \
-	v8/src/string-search.cc \
-	v8/src/string-stream.cc \
-	v8/src/strtod.cc \
-	v8/src/stub-cache.cc \
-	v8/src/sweeper-thread.cc \
-	v8/src/token.cc \
-	v8/src/transitions.cc \
-	v8/src/type-info.cc \
-	v8/src/types.cc \
-	v8/src/typing.cc \
-	v8/src/unicode.cc \
-	v8/src/utils.cc \
-	v8/src/v8.cc \
-	v8/src/v8threads.cc \
-	v8/src/variables.cc \
-	v8/src/version.cc \
-	v8/src/zone.cc \
-	v8/src/arm/assembler-arm.cc \
-	v8/src/arm/builtins-arm.cc \
-	v8/src/arm/code-stubs-arm.cc \
-	v8/src/arm/codegen-arm.cc \
-	v8/src/arm/constants-arm.cc \
-	v8/src/arm/cpu-arm.cc \
-	v8/src/arm/debug-arm.cc \
-	v8/src/arm/deoptimizer-arm.cc \
-	v8/src/arm/disasm-arm.cc \
-	v8/src/arm/frames-arm.cc \
-	v8/src/arm/full-codegen-arm.cc \
-	v8/src/arm/ic-arm.cc \
-	v8/src/arm/lithium-arm.cc \
-	v8/src/arm/lithium-codegen-arm.cc \
-	v8/src/arm/lithium-gap-resolver-arm.cc \
-	v8/src/arm/macro-assembler-arm.cc \
-	v8/src/arm/regexp-macro-assembler-arm.cc \
-	v8/src/arm/simulator-arm.cc \
-	v8/src/arm/stub-cache-arm.cc
+	v8/src/libplatform/default-platform.cc \
+	v8/src/libplatform/task-queue.cc \
+	v8/src/libplatform/worker-thread.cc
 
 
 # Flags passed to both C and C++ files.
@@ -239,9 +80,6 @@ MY_DEFS_Debug := \
 	'-DV8_TARGET_ARCH_ARM' \
 	'-DCAN_USE_ARMV7_INSTRUCTIONS' \
 	'-DV8_I18N_SUPPORT' \
-	'-DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_STATIC' \
-	'-DU_USING_ICU_NAMESPACE=0' \
-	'-DU_STATIC_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
 	'-DDYNAMIC_ANNOTATIONS_ENABLED=1' \
@@ -258,9 +96,7 @@ MY_DEFS_Debug := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Debug := \
 	$(LOCAL_PATH)/v8 \
-	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/icu/source/i18n \
-	$(LOCAL_PATH)/third_party/icu/source/common
+	$(gyp_shared_intermediate_dir)
 
 
 # Flags passed to only C++ (and not C) files.
@@ -323,9 +159,6 @@ MY_DEFS_Release := \
 	'-DV8_TARGET_ARCH_ARM' \
 	'-DCAN_USE_ARMV7_INSTRUCTIONS' \
 	'-DV8_I18N_SUPPORT' \
-	'-DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_STATIC' \
-	'-DU_USING_ICU_NAMESPACE=0' \
-	'-DU_STATIC_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
 	'-DNDEBUG' \
@@ -337,9 +170,7 @@ MY_DEFS_Release := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Release := \
 	$(LOCAL_PATH)/v8 \
-	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/icu/source/i18n \
-	$(LOCAL_PATH)/third_party/icu/source/common
+	$(gyp_shared_intermediate_dir)
 
 
 # Flags passed to only C++ (and not C) files.
@@ -385,10 +216,10 @@ LOCAL_SHARED_LIBRARIES :=
 
 # Add target alias to "gyp_all_modules" target.
 .PHONY: gyp_all_modules
-gyp_all_modules: v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+gyp_all_modules: v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 
 # Alias gyp target name.
-.PHONY: v8_base
-v8_base: v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+.PHONY: v8_libplatform
+v8_libplatform: v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 
 include $(BUILD_HOST_STATIC_LIBRARY)
--- tools/gyp/v8_libplatform.host.linux-mips.mk
+++ tools/gyp/v8_libplatform.host.linux-mips.mk
@@ -3,7 +3,7 @@
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_CLASS := STATIC_LIBRARIES
-LOCAL_MODULE := v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+LOCAL_MODULE := v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 LOCAL_MODULE_SUFFIX := .a
 LOCAL_MODULE_TAGS := optional
 LOCAL_IS_HOST_MODULE := true
@@ -25,168 +25,9 @@ LOCAL_GENERATED_SOURCES :=
 GYP_COPIED_SOURCE_ORIGIN_DIRS :=
 
 LOCAL_SRC_FILES := \
-	v8/src/accessors.cc \
-	v8/src/allocation.cc \
-	v8/src/allocation-site-scopes.cc \
-	v8/src/allocation-tracker.cc \
-	v8/src/api.cc \
-	v8/src/arguments.cc \
-	v8/src/assembler.cc \
-	v8/src/assert-scope.cc \
-	v8/src/ast-value-factory.cc \
-	v8/src/ast.cc \
-	v8/src/bignum-dtoa.cc \
-	v8/src/bignum.cc \
-	v8/src/bootstrapper.cc \
-	v8/src/builtins.cc \
-	v8/src/cached-powers.cc \
-	v8/src/checks.cc \
-	v8/src/code-stubs.cc \
-	v8/src/code-stubs-hydrogen.cc \
-	v8/src/codegen.cc \
-	v8/src/compilation-cache.cc \
-	v8/src/compiler.cc \
-	v8/src/contexts.cc \
-	v8/src/conversions.cc \
-	v8/src/counters.cc \
-	v8/src/cpu-profiler.cc \
-	v8/src/data-flow.cc \
-	v8/src/date.cc \
-	v8/src/dateparser.cc \
-	v8/src/debug.cc \
-	v8/src/deoptimizer.cc \
-	v8/src/disassembler.cc \
-	v8/src/diy-fp.cc \
-	v8/src/dtoa.cc \
-	v8/src/elements-kind.cc \
-	v8/src/elements.cc \
-	v8/src/execution.cc \
-	v8/src/extensions/externalize-string-extension.cc \
-	v8/src/extensions/free-buffer-extension.cc \
-	v8/src/extensions/gc-extension.cc \
-	v8/src/extensions/statistics-extension.cc \
-	v8/src/extensions/trigger-failure-extension.cc \
-	v8/src/factory.cc \
-	v8/src/fast-dtoa.cc \
-	v8/src/field-index.cc \
-	v8/src/fixed-dtoa.cc \
-	v8/src/flags.cc \
-	v8/src/frames.cc \
-	v8/src/full-codegen.cc \
-	v8/src/func-name-inferrer.cc \
-	v8/src/gdb-jit.cc \
-	v8/src/global-handles.cc \
-	v8/src/handles.cc \
-	v8/src/heap-profiler.cc \
-	v8/src/heap-snapshot-generator.cc \
-	v8/src/heap.cc \
-	v8/src/hydrogen-bce.cc \
-	v8/src/hydrogen-bch.cc \
-	v8/src/hydrogen-canonicalize.cc \
-	v8/src/hydrogen-check-elimination.cc \
-	v8/src/hydrogen-dce.cc \
-	v8/src/hydrogen-dehoist.cc \
-	v8/src/hydrogen-environment-liveness.cc \
-	v8/src/hydrogen-escape-analysis.cc \
-	v8/src/hydrogen-instructions.cc \
-	v8/src/hydrogen.cc \
-	v8/src/hydrogen-gvn.cc \
-	v8/src/hydrogen-infer-representation.cc \
-	v8/src/hydrogen-infer-types.cc \
-	v8/src/hydrogen-load-elimination.cc \
-	v8/src/hydrogen-mark-deoptimize.cc \
-	v8/src/hydrogen-mark-unreachable.cc \
-	v8/src/hydrogen-osr.cc \
-	v8/src/hydrogen-range-analysis.cc \
-	v8/src/hydrogen-redundant-phi.cc \
-	v8/src/hydrogen-removable-simulates.cc \
-	v8/src/hydrogen-representation-changes.cc \
-	v8/src/hydrogen-sce.cc \
-	v8/src/hydrogen-store-elimination.cc \
-	v8/src/hydrogen-types.cc \
-	v8/src/hydrogen-uint32-analysis.cc \
-	v8/src/i18n.cc \
-	v8/src/icu_util.cc \
-	v8/src/ic.cc \
-	v8/src/incremental-marking.cc \
-	v8/src/interface.cc \
-	v8/src/interpreter-irregexp.cc \
-	v8/src/isolate.cc \
-	v8/src/jsregexp.cc \
-	v8/src/lithium-allocator.cc \
-	v8/src/lithium-codegen.cc \
-	v8/src/lithium.cc \
-	v8/src/liveedit.cc \
-	v8/src/log-utils.cc \
-	v8/src/log.cc \
-	v8/src/lookup.cc \
-	v8/src/mark-compact.cc \
-	v8/src/messages.cc \
-	v8/src/objects-debug.cc \
-	v8/src/objects-printer.cc \
-	v8/src/objects-visiting.cc \
-	v8/src/objects.cc \
-	v8/src/optimizing-compiler-thread.cc \
-	v8/src/ostreams.cc \
-	v8/src/parser.cc \
-	v8/src/preparse-data.cc \
-	v8/src/preparser.cc \
-	v8/src/prettyprinter.cc \
-	v8/src/profile-generator.cc \
-	v8/src/property.cc \
-	v8/src/regexp-macro-assembler-irregexp.cc \
-	v8/src/regexp-macro-assembler-tracer.cc \
-	v8/src/regexp-macro-assembler.cc \
-	v8/src/regexp-stack.cc \
-	v8/src/rewriter.cc \
-	v8/src/runtime-profiler.cc \
-	v8/src/runtime.cc \
-	v8/src/safepoint-table.cc \
-	v8/src/sampler.cc \
-	v8/src/scanner-character-streams.cc \
-	v8/src/scanner.cc \
-	v8/src/scopeinfo.cc \
-	v8/src/scopes.cc \
-	v8/src/serialize.cc \
-	v8/src/snapshot-source-sink.cc \
-	v8/src/spaces.cc \
-	v8/src/store-buffer.cc \
-	v8/src/string-search.cc \
-	v8/src/string-stream.cc \
-	v8/src/strtod.cc \
-	v8/src/stub-cache.cc \
-	v8/src/sweeper-thread.cc \
-	v8/src/token.cc \
-	v8/src/transitions.cc \
-	v8/src/type-info.cc \
-	v8/src/types.cc \
-	v8/src/typing.cc \
-	v8/src/unicode.cc \
-	v8/src/utils.cc \
-	v8/src/v8.cc \
-	v8/src/v8threads.cc \
-	v8/src/variables.cc \
-	v8/src/version.cc \
-	v8/src/zone.cc \
-	v8/src/mips/assembler-mips.cc \
-	v8/src/mips/builtins-mips.cc \
-	v8/src/mips/codegen-mips.cc \
-	v8/src/mips/code-stubs-mips.cc \
-	v8/src/mips/constants-mips.cc \
-	v8/src/mips/cpu-mips.cc \
-	v8/src/mips/debug-mips.cc \
-	v8/src/mips/deoptimizer-mips.cc \
-	v8/src/mips/disasm-mips.cc \
-	v8/src/mips/frames-mips.cc \
-	v8/src/mips/full-codegen-mips.cc \
-	v8/src/mips/ic-mips.cc \
-	v8/src/mips/lithium-codegen-mips.cc \
-	v8/src/mips/lithium-gap-resolver-mips.cc \
-	v8/src/mips/lithium-mips.cc \
-	v8/src/mips/macro-assembler-mips.cc \
-	v8/src/mips/regexp-macro-assembler-mips.cc \
-	v8/src/mips/simulator-mips.cc \
-	v8/src/mips/stub-cache-mips.cc
+	v8/src/libplatform/default-platform.cc \
+	v8/src/libplatform/task-queue.cc \
+	v8/src/libplatform/worker-thread.cc
 
 
 # Flags passed to both C and C++ files.
@@ -202,6 +43,7 @@ MY_CFLAGS_Debug := \
 	-fvisibility=hidden \
 	-pipe \
 	-fPIC \
+	-Wno-unused-local-typedefs \
 	-Wno-format \
 	-m32 \
 	-Os \
@@ -241,9 +83,6 @@ MY_DEFS_Debug := \
 	'-D__mips_hard_float=1' \
 	'-D_MIPS_ARCH_MIPS32R2' \
 	'-DV8_I18N_SUPPORT' \
-	'-DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_STATIC' \
-	'-DU_USING_ICU_NAMESPACE=0' \
-	'-DU_STATIC_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
 	'-DDYNAMIC_ANNOTATIONS_ENABLED=1' \
@@ -260,9 +99,7 @@ MY_DEFS_Debug := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Debug := \
 	$(LOCAL_PATH)/v8 \
-	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/icu/source/i18n \
-	$(LOCAL_PATH)/third_party/icu/source/common
+	$(gyp_shared_intermediate_dir)
 
 
 # Flags passed to only C++ (and not C) files.
@@ -286,6 +123,7 @@ MY_CFLAGS_Release := \
 	-fvisibility=hidden \
 	-pipe \
 	-fPIC \
+	-Wno-unused-local-typedefs \
 	-Wno-format \
 	-m32 \
 	-fno-ident \
@@ -327,9 +165,6 @@ MY_DEFS_Release := \
 	'-D__mips_hard_float=1' \
 	'-D_MIPS_ARCH_MIPS32R2' \
 	'-DV8_I18N_SUPPORT' \
-	'-DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_STATIC' \
-	'-DU_USING_ICU_NAMESPACE=0' \
-	'-DU_STATIC_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
 	'-DNDEBUG' \
@@ -341,9 +176,7 @@ MY_DEFS_Release := \
 # Include paths placed before CFLAGS/CPPFLAGS
 LOCAL_C_INCLUDES_Release := \
 	$(LOCAL_PATH)/v8 \
-	$(gyp_shared_intermediate_dir) \
-	$(LOCAL_PATH)/third_party/icu/source/i18n \
-	$(LOCAL_PATH)/third_party/icu/source/common
+	$(gyp_shared_intermediate_dir)
 
 
 # Flags passed to only C++ (and not C) files.
@@ -363,12 +196,16 @@ LOCAL_ASFLAGS := $(LOCAL_CFLAGS)
 ### Rules for final target.
 
 LOCAL_LDFLAGS_Debug := \
+	-Wl,-z,now \
+	-Wl,-z,relro \
 	-pthread \
 	-fPIC \
 	-m32
 
 
 LOCAL_LDFLAGS_Release := \
+	-Wl,-z,now \
+	-Wl,-z,relro \
 	-pthread \
 	-fPIC \
 	-m32
@@ -385,10 +222,10 @@ LOCAL_SHARED_LIBRARIES :=
 
 # Add target alias to "gyp_all_modules" target.
 .PHONY: gyp_all_modules
-gyp_all_modules: v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+gyp_all_modules: v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 
 # Alias gyp target name.
-.PHONY: v8_base
-v8_base: v8_tools_gyp_v8_base_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
+.PHONY: v8_libplatform
+v8_libplatform: v8_tools_gyp_v8_libplatform_$(TARGET_$(GYP_VAR_PREFIX)ARCH)_host_gyp
 
 include $(BUILD_HOST_STATIC_LIBRARY)
