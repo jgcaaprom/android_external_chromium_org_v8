59024954719c634f52fec4e9e0c18a3066801b7e
Android Chromium Automerger <chromium-automerger@android>
2014-09-09 14:33:19 +0000

    Update makefiles after merge of Chromium at facf66e09bf8
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I908d3b3bb70c7eaa455c81dad4a2404d44796c25

--- tools/gyp/v8_base.target.darwin-arm.mk
+++ tools/gyp/v8_base.target.darwin-arm.mk
@@ -255,6 +255,11 @@ MY_CFLAGS_Debug := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
@@ -368,6 +373,11 @@ MY_CFLAGS_Release := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
--- tools/gyp/v8_libbase.target.darwin-arm.mk
+++ tools/gyp/v8_libbase.target.darwin-arm.mk
@@ -56,6 +56,11 @@ MY_CFLAGS_Debug := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
@@ -163,6 +168,11 @@ MY_CFLAGS_Release := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
