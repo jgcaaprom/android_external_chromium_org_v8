eba9304aedaf5734919f0936f7ff1876cad04200
Tony Layher <layhertony@gmail.com>
2015-04-05 12:24:44 -0400

    Fix build for MacOSX 10.10.X and CLI Tools 6.1.1
    Change-Id: Ic2052cd54e8c07f595ea273673322dace81ce760

--- tools/gyp/mksnapshot.host.darwin-arm64.mk
+++ tools/gyp/mksnapshot.host.darwin-arm64.mk
@@ -208,13 +208,13 @@ LOCAL_ASFLAGS := $(LOCAL_CFLAGS)
 ### Rules for final target.
 
 LOCAL_LDFLAGS_Debug := \
-	-pthread \
+	-Wl,-lstdc++ \
 	-fPIC \
 	-m64
 
 
 LOCAL_LDFLAGS_Release := \
-	-pthread \
+	-Wl,-lstdc++ \
 	-fPIC \
 	-m64
 
